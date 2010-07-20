dd0c9bbb1a619fb329775c34b7483acc9d6e8beb
Mark Meves <mark.meves@gmail.com>
2010-07-20 17:05:01 -0400

    * add: stupid simple CR of CRUD

---
+++ app/controllers/parts_controller.rb
@@ -0,0 +1,29 @@
+class PartsController < ApplicationController
+  
+  def create
+    puts "thing: #{params.inspect}"
+    # @todo validation on dups, invalid/no name, etc
+    gadget_id = params[:gadget_id]
+    name = params[:part_name]
+    part = Part.new(:name => name, :gadget_id => gadget_id)
+    part.save!
+    redirect_to "/gadgets/#{gadget_id}/detail"
+  end
+  
+  def index
+    fetch_gadget
+    @parts = @gadget.parts    
+  end  
+
+  def new
+    fetch_gadget
+    @gadget_name = @gadget.name
+    @gadget_id = params[:gadget_id]
+  end
+
+  protected
+  def fetch_gadget
+    @gadget_id = params[:gadget_id]
+    @gadget = Gadget.find(params[:gadget_id])
+  end
+end
---
+++ app/models/part.rb
@@ -0,0 +1,3 @@
+class Part < ActiveRecord::Base
+  has_one :gadget
+end
